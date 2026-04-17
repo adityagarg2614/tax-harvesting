fa787fef8359e7a29230336c2f82836a6127721b
adityagarg2614 <adityagarg065@gmail.com>
2026-04-17 19:13:17 +0530

    added dummy data for showing on UI

---
+++ src/data/disclaimer.ts
@@ -0,0 +1,6 @@
+export const disclaimer = [
+    "Tax-loss harvesting is currently not allowed under Indian tax regulations.",
+    "Tax harvesting does not apply to derivatives or futures.",
+    "Prices may differ slightly from exchanges.",
+    "Only realized losses are counted.",
+];
---
+++ src/data/summary.ts
@@ -0,0 +1,14 @@
+export const preHarvest = {
+    profits: "$1,540",
+    losses: "-$743",
+    net: "$787",
+    realized: "$1,337",
+};
+
+export const afterHarvest = {
+    profits: "$1,540",
+    losses: "-$2,343",
+    net: "-$987",
+    effective: "-$2,353",
+    saved: "$862",
+};
